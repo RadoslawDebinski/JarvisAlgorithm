60a2367c69d69a5592fd8fff7cc7197cb42c4a08
RadoslawDebinski <radek.debinski@wp.pl>
2023-01-09 21:12:04 +0100

    hitbox should be added

--- JarvisAlgorithm.py
+++ JarvisAlgorithm.py
@@ -2,57 +2,50 @@ import numpy as np
 
 class JarAlg():
     def __init__(self, pointsx, pointsy):
-        self.pointsx = pointsx
-        self.pointsy = pointsy
+        self.pointsx = np.concatenate(pointsx, axis = 0)
+        self.pointsy = np.concatenate(pointsy, axis = 0)
+        # self.pointsx = np.stack(pointsx)
+        # self.pointsy = np.stack(pointsy)
 
-    def _findStart(self):
+    def findStart(self):
         startId, iter = int(0), int(0)
         for i in self.pointsy:
             if i < self.pointsy[startId]:
                 startId = iter
             iter += 1
-        return startId
+        return [self.pointsx[startId], self.pointsy[startId]]
 
-    def _findTop(self):
+    def _findTop(self, start):
+        self.start
+        tempPoitsY = list(self.pointsy)
+        tempPoitsY.append(start[1])
         topId, iter = int(0), int(0)
-        for i in self.pointsy:
-            if i > self.pointsy[topId]:
+        for i in tempPoitsY:
+            if i > tempPoitsY[topId]:
                 topId = iter
             iter += 1
         return topId
 
 
-    def calculate(self):
-        startId = JarAlg._findStart(self)
-        topId = JarAlg._findTop(self)
+    def calculate(self, start):
+        self.start = start
+        topId = JarAlg._findTop(self, self.start)
         topRiched = False
 
-        hullX, hullY = [], []
-        hullX.append(self.pointsx[startId])
-        hullY.append(self.pointsy[startId])
+        angles = []
+        for i in range(0,len(self.pointsy)):
+            angles.append(np.arctan2(self.pointsy[i] - start[1], self.pointsx[i] - start[0]))
 
-        while True:
-            angles = []
-            for i in range(0,len(self.pointsy)):
-                angles.append(np.arctan2(self.pointsy[i] - self.pointsy[startId], self.pointsx[i] - self.pointsx[startId]))
+        if topId == len(self.pointsy):
+            topRiched = True
+        if topRiched:
+            nextId = np.argmin(angles)
+        else:
+            angles = np.array(angles)
+            moreZeroAnglesId = angles > 0.0
+            minVal = np.amin(angles[moreZeroAnglesId == True])
+            nextId = int(np.where(angles == minVal)[0])
 
-            if self.pointsy[startId] == self.pointsy[topId]:
-                topRiched = True
-            if topRiched:
-                nextId = np.argmin(angles)
-            else:
-                angles = np.array(angles)
-                moreZeroAnglesId = angles > 0.0
-                minVal = np.amin(angles[moreZeroAnglesId == True])
-                nextId = int(np.where(angles == minVal)[0])
-
-                
-            if nextId == startId:
-                break
-            hullX.append(self.pointsx[nextId])
-            hullY.append(self.pointsy[nextId])
-            startId = nextId
-
-        return hullX, hullY
+        return [self.pointsx[nextId], self.pointsy[nextId]]
 
 
--- Lab2.py
+++ Lab2.py
@@ -16,47 +16,58 @@ ptX, ptY = [], []
 for _ in range (0,pointsAmount):
     point = pt.Point(xSpan,ySpan, triangleBase, triangleH)
     ptStruct.append(point)
-    ptX.append(point.xPos)
-    ptY.append(point.yPos)
-    ptX.append(point.xPos1)
-    ptY.append(point.yPos1)
-    ptX.append(point.xPos2)
-    ptY.append(point.yPos2)
-
-JA = ja.JarAlg(ptX,ptY)
-hullX, hullY = JA.calculate()
 
 fig, ax = plt.subplots()
 ax.set_xlim(-xSpan-frame, xSpan+frame)
 ax.set_ylim(-ySpan-frame, ySpan+frame)
 
-#points, = ax.plot(ptX, ptY, 'bo', markersize=2)
+
 hullDataX = []
 hullDataY = []
-#hullData, = ax.plot([], [], marker='o', color='green', markersize=3)
+hull, = ax.plot([], [], marker='o', color='green', markersize=2)
 triangles = []
 for _ in range(0, pointsAmount):
-    triangles.append(ax.plot([], [], marker='o', color='black', markersize=3)[0])
+    triangles.append(ax.plot([], [], marker='o', color='black', markersize=1)[0])
+
+start = []
+firstLatch = True
 
 def anime(frame):
-    triX = []
-    triY = []
+    global start
+    global firstLatch
+    triX, pointsX = [[], []]
+    triY, pointsY = [[], []]
 
     i = 0
     for point in ptStruct:
         point = point + point.movVect
         ptStruct[i] = point
         triX.append([point.xPos, point.xPos1, point.xPos2, point.xPos])
+        pointsX.append([point.xPos, point.xPos1, point.xPos2])
         triY.append([point.yPos, point.yPos1, point.yPos2, point.yPos])
+        pointsY.append([point.yPos, point.yPos1, point.yPos2])
         i += 1
 
     for x, y, i in zip(triX, triY, range(0, pointsAmount)):
         triangles[i].set_data(x, y)
-    # hullDataX.append(hullX[frame])
-    # hullDataY.append(hullY[frame])
-    # hulldata.set_data(hullDataX, hullDataY)
 
-anim = FuncAnimation(fig, anime, frames=400, interval=100)
+    hullDataX, hullDataY = [list(hull.get_xdata()), list(hull.get_ydata())]
+    JA = ja.JarAlg(np.array(pointsX),np.array(pointsY))
+    if firstLatch:
+        start = JA.findStart()
+        hullX, hullY = start[0], start[1]
+        firstLatch = False
+        hullDataX.append(hullX)
+        hullDataY.append(hullY)
+        hull.set_data(hullDataX, hullDataY)
+    else:
+        start = [hullDataX[-1],hullDataY[-1]]
+        hullX, hullY = JA.calculate(start)
+        hull.set_data(np.concatenate([hullDataX, np.linspace(hullDataX[-1],hullX, 200)]),
+                      np.concatenate([hullDataY, np.linspace(hullDataY[-1],hullY, 200)]))
+
+
+anim = FuncAnimation(fig, anime, frames=400, interval=500)
 plt.title('Second laboratory WNO')
 plt.xlabel('X Axis')
 plt.ylabel('Y Axis')
